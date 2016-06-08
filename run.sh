dbe1d59ca9704ea673651fb3e987d6a7de1505bf
Sergi <sergibernaus@yahoo.es>
2016-06-08 23:10:36 +0000

    SQLite fixed (yay)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Chii Started
-nohup forever -a -o /home/pi/Documents/Projects/logs/chii/out.log -e /home/pi/Documents/Projects/logs/chii/err.log start -w --watchDirectory /home/pi/Documents/Projects/chii --watchIgnore /home/pi/Documents/Projects/chii/.git --watchIgnore /home/pi/Documents/Projects/chii/sqldb.db --killSignal=SIGTERM --exitcrash --uid "chii" /home/pi/Documents/Projects/chii/bin/bot.js >> /home/pi/Documents/Projects/logs/chii/nohup.out &
+nohup forever -a -o /home/pi/Documents/Projects/logs/chii/out.log -e /home/pi/Documents/Projects/logs/chii/err.log start -w --watchDirectory /home/pi/Documents/Projects/chii --watchIgnore /home/pi/Documents/Projects/chii/.git --watchIgnore /home/pi/Documents/Projects/chii/sqlite --killSignal=SIGTERM --exitcrash --uid "chii" /home/pi/Documents/Projects/chii/bin/bot.js >> /home/pi/Documents/Projects/logs/chii/nohup.out &
 
