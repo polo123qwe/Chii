3d9cf3ccaf52883f7908a70da379e1d5ce4dfb7b
Sergi B <sergibernaus@yahoo.es>
2016-08-27 02:14:01 +0300

    Fixed typo and added randu

---
+++ data/commands/fun.js
@@ -0,0 +1,38 @@
+var CommandArray = [];
+var utilsLoader = require('../../utils/utilsLoader.js');
+var clog = utilsLoader.clog;
+var utils = utilsLoader.generalUtils;
+var db = utilsLoader.db;
+var config = require('../../config.json');
+
+CommandArray.randu = {
+    name: 'randu',
+    help: "Returns a random user",
+    cooldown: 5,
+    levelReq: 0,
+    clean: 1,
+    exec: function(client, msg, suffix) {
+        if (msg.isPrivate) return;
+        var guild = msg.guild;
+
+        var guildUsers = guild.members;
+        //Select a random user
+        for (var user of guildUsers) {
+            var found = false;
+            var roles = user.roles;
+            if(roles.find(r => {r.name == "Member"})){
+                found = true;
+            }
+            if (!found) {
+                var index = guildUsers.indexOf(user);
+                guildUsers.splice(index, 1);
+            }
+        }
+        var randomUser = guildUsers[utils.getRandom(0, guildUsers.length-1)];
+
+        msg.channel.sendMessage(randomUser.name + "!");
+    }
+}
+
+
+exports.CommandArray = CommandArray;
